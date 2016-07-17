a9bb9a54566cce3f8adeb439b2dfd5db4448a0f7
Shas <shaswarbaban@gmail.com>
2016-07-17 13:27:44 +0100

    ./trustful-api-master/application/api/polls/get_results.js created at Jul 17 13:27:44 2016

---
+++ trustful-api-master/application/api/polls/get_results.js
@@ -0,0 +1,38 @@
+'use strict'
+
+let Polls = require('../../trustful/polls')
+
+module.exports = server => {
+  server.get(endpoint, (req, res, next) => {
+
+    let id = req.params.id
+    let userId = req.params.userId
+
+    Polls.find(id)
+      .then(poll => {
+        return poll.results.then(details => {
+          return poll.checkVoted(userId).then(hasVoted => {
+            details.hasVoted = hasVoted
+            return details
+          })
+        })
+      })
+      .then(poll => res.send(poll))
+      .catch(err => res.send(err))
+  }) 
+}
+
+let endpoint = {
+  path: '/polls/:id/results',
+  version: '1.0.0',
+  params: {
+    id: {
+      dataTypes: ['number'],
+      required: true
+    },
+    userId: {
+      dataTypes: ['number'],
+      required: true
+    }
+  }
+}
